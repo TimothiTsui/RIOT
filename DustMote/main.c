7c41f6b4f186ca458a79bce64b1e1a3cfca953c6
skull <skull@IgorsThinkpadP51>
2018-05-14 13:26:15 +0200

    openthread update

--- DustMote/main.c
+++ DustMote/main.c
@@ -6,13 +6,10 @@
 int main(void){
     //puts("Hello test!");
 
-<<<<<<< HEAD
+
     printf("You are running RIOT on a(n) %s board.\n",  BOARD_REMOTE_REVB);
     printf("This board features a(n) %s MCU.\n", RIOT_MCU);
-=======
-    printf("You are running RIOT on a(n) %s board.\n",  BOARD_REMOTE_REVB); // @suppress("Symbol is not resolved")
-    printf("TEST This board features a(n) %s MCU.\n", RIOT_MCU);  // @suppress("Symbol is not resolved")
->>>>>>> branch 'master' of https://github.com/skullbox305/RIOT.git
+
 
     //GPIO_NUM_TO_DEV(GPIO_PC5);
 //      gpio_init(GPIO_PIN(2,5), GPIO_OUT);
