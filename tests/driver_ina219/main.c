b737bff36ad595fcc7ac475537cca2747253821a
skull <skull@IgorsThinkpadP51>
2018-05-15 22:12:24 +0200

    changed INA219 configuration in test script

--- tests/driver_ina219/main.c
+++ tests/driver_ina219/main.c
@@ -37,67 +37,70 @@
  *  - 32 V maximum bus voltage
  *  - 12 bit ADC resolution, no hardware averaging
  */
-#define CONFIG   (INA219_MODE_CONTINUOUS_SHUNT_BUS | INA219_RANGE_40MV | \
+#define CONFIG   (INA219_MODE_CONTINUOUS_SHUNT_BUS | INA219_RANGE_320MV | \
                   INA219_BRNG_16V_FSR | INA219_SADC_12BIT | INA219_BADC_12BIT)
-#define CALIBRATION (4096)
-#define SLEEP    (100 * 1000U)
+#define CALIBRATION (10240)
+#define SLEEP    (100 * 20000U)
 
-int main(void)
-{
+int main(void){
     ina219_t dev;
     int16_t val;
 
     puts("INA219 sensor driver test application\n");
     printf("Initializing I2C_%i... ", TEST_INA219_I2C);
-    if (i2c_init_master(TEST_INA219_I2C, I2C_SPEED_FAST) < 0) {
+    if(i2c_init_master(TEST_INA219_I2C, I2C_SPEED_FAST) < 0) {
         return -1;
     }
 
     printf("Initializing INA219 sensor at I2C_%i, address 0x%02x... ",
-        TEST_INA219_I2C, TEST_INA219_ADDR);
-    if (ina219_init(&dev, TEST_INA219_I2C, TEST_INA219_ADDR) == 0) {
+            TEST_INA219_I2C, TEST_INA219_ADDR);
+    if(ina219_init(&dev, TEST_INA219_I2C, TEST_INA219_ADDR) == 0) {
         puts("[OK]\n");
-    } else {
+    }
+    else {
         puts("[Failed]");
         return 1;
     }
     puts("Set configuration register");
-    if (ina219_set_config(&dev, CONFIG) == 0) {
+    if(ina219_set_config(&dev, CONFIG) == 0) {
         puts("[OK]\n");
-    } else {
+    }
+    else {
         puts("[Failed]");
         return 1;
     }
 
     puts("Set calibration register");
-    if (ina219_set_calibration(&dev, CALIBRATION) == 0) {
+    if(ina219_set_calibration(&dev, CALIBRATION) == 0) {
         puts("[OK]\n");
-    } else {
+    }
+    else {
         puts("[Failed]");
         return 1;
     }
 
-    while (1) {
+    while(1) {
         /* Read shunt resistor voltage, in millivolts */
         ina219_read_shunt(&dev, &val);
-        printf("shunt: %6d", val);
+        printf("shunt: %6d mV", (val));
 
-        /* Read VBUS voltage, in millivolts */
+        /* Read VBUS voltage, in volts */
         ina219_read_bus(&dev, &val);
         /* The bus voltage is found in the topmost 13 bits of the bus voltage
          * register */
-        val = (val >> INA219_BUS_VOLTAGE_SHIFT);
-        printf("\tbus: %6d", val);
+        val = (val >> INA219_BUS_VOLTAGE_SHIFT) * 4;
+        printf("\tbus: %6d V", (val));
 
-        /* Read current register, the scale depends on the value of the
+
+        /* Read current register milliampere, the scale depends on the value of the
          * calibration register */
         ina219_read_current(&dev, &val);
-        printf("\tcurrent: %6d", val);
+        printf("\tcurrent: %6d mA", (val / 25));
 
-        /* Read power register, the scale depends on the value of the
+        /* Read power register in watts, the scale depends on the value of the
          * calibration register */
         ina219_read_power(&dev, &val);
-        printf("\tpower: %6d\n", val);
+        printf("\tpower: %6d mW\n", (val));
 
         xtimer_usleep(SLEEP);
     }
